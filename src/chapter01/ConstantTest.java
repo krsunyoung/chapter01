cdb224e51c04285ee10fbb22924442315e878067
sunyoung <bit-user1@bit-user>
2016-09-05 17:21:15 +0900

    commit

--- src/chapter01/ConstantTest.java
+++ src/chapter01/ConstantTest.java
@@ -4,7 +4,7 @@ public class ConstantTest {
 	public static void main(String[] args){
 		final int CONST_NUMBER = 10;
 		final double PI = 3.14;
-		//많은 곳에 사용하는 값을 한곳만 변경하면 되서 수월해서 많이 사용한다.
+		
 		
 		int number = 10;
 		System.out.println(number);
@@ -12,7 +12,7 @@ public class ConstantTest {
 		number = 20;
 		System.out.println(number);
 		
-		//변수로 값을 변경할수 있지만  상수는 값을 변경 할수 없다. 상수는 대문자로 해주는것이 관례  
+		
 		
 		System.out.println(CONST_NUMBER);
 
