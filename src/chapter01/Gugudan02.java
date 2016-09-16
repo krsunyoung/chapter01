52829ef14687695d77e2361e013cfd0bf2231046
박선영 <@172.30.1.29>
2016-09-16 20:09:46 +0900

    0916

---
+++ src/chapter01/Gugudan02.java
@@ -0,0 +1,22 @@
+package chapter01;
+
+public class Gugudan02 {
+
+	public static void main(String[] args) {
+		int result = 0;
+		
+		for (int i= 1; i<=9; i++){
+			for (int j =1; j<=9; j++){
+				result = i * j;
+
+				
+				System.out.print(i +"*"+ j +"="+result+"\t");
+				
+			}System.out.println(" ");
+
+		}
+
+
+	}
+
+}
